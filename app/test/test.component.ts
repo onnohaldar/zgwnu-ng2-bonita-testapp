6dc03edcddeca452288f655143ca68d6b1b68d99
Onno Haldar <onno@zgw.nu>
2017-01-12 18:07:24 +0100

    setup create case (not working yet)

--- app/test/test.component.ts
+++ app/test/test.component.ts
@@ -1,8 +1,14 @@
 import {Component, OnInit} from '@angular/core'
 
-import { BonitaErrorResponse, 
+import { BonitaResponse ,BonitaErrorResponse, 
   BonitaAuthenticationService, BonitaSession, 
-  BonitaBpmProcessService, BonitaSearchParms, BonitaProcessDefinition } from '../zgwnu2/bonita'
+  BonitaBpmProcessService, BonitaSearchParms, BonitaProcessDefinition, BonitaCreateCaseSuccessResponse 
+  } from '../zgwnu2/bonita'
+
+import { CreateCaseContract } from './create-case-contract'
+import { Ng2BonitaMasterContract } from './ng2-bonita-master-contract'
+import { Ng2BonitaDetailContract } from './ng2-bonita-detail-contract'
+import { Ng2BonitaXrefContract } from './ng2-bonita-xref-contract'
 
 @Component({
   moduleId: module.id,
@@ -14,6 +20,7 @@ import { BonitaErrorResponse,
 export class TestComponent implements OnInit {
 
   // generic bonita rest api test vars
+  response: BonitaResponse
   errorResponse: BonitaErrorResponse
 
   // authentication test vars
@@ -23,6 +30,24 @@ export class TestComponent implements OnInit {
   // bpm process test vars
   processDefinition: BonitaProcessDefinition
   passedTestBPMProcessSearchProcessDefinitions: boolean = false
+  passedTestBPMProcessGetProcessDefinition: boolean = false
+  passedTestBPMProcessCreateCase: boolean = false
+  createCaseData: any = {
+    masterKey: 'MasterKeyValue1',
+    masterDate: new Date(2017, 0, 12),
+    details: [
+      {detailKey: 'detailTestKey1A', xrefKey: 'xrefTestKeyAA'},
+      {detailKey: 'detailTestKey2B', xrefKey: 'xrefTestKeyBB'},
+      {detailKey: 'detailTestKey3C', xrefKey: 'xrefTestKeyCC'},
+    ],
+    masterBoolean: true,
+    masterDouble: Number.MAX_VALUE,
+    masterFloat: 1000.327436,
+    masterInteger: 10000,
+    masterLong: 100000,
+    masterText: 'This is a test Text for zgwnu-ng2-bonita-testapp'
+  }
+  createCaseSuccessResponse: BonitaCreateCaseSuccessResponse
 
   constructor(
     private authenticationService: BonitaAuthenticationService, 
@@ -46,7 +71,7 @@ export class TestComponent implements OnInit {
     this.session = this.authenticationService.session
     if (this.session) { 
       this.passedTestAuthenticationSession = true 
-      // next test in chain
+      // next test in chain (1)
       this.testBPMProcessSearchProcessDefinitions()
     }
   }
@@ -61,9 +86,38 @@ export class TestComponent implements OnInit {
         processDefinitions => {
           this.processDefinition = processDefinitions[0]
           this.passedTestBPMProcessSearchProcessDefinitions = true
+          // next test in chain (2)
+          this.testBPMProcessGetProcessDefinition()
+        },
+        errorResponse => this.errorResponse = errorResponse
+      )
+  }
+
+  private testBPMProcessGetProcessDefinition() {
+    this.bpmProcessService.getProcessDefinition(this.processDefinition.id)
+      .subscribe(
+        processDefinition => {
+          this.processDefinition = processDefinition
+          this.passedTestBPMProcessGetProcessDefinition = true
+          // next test in chain (3)
+          this.testBPMProcessCreateCase()
         },
         errorResponse => this.errorResponse = errorResponse
       )
   }
   
+  private testBPMProcessCreateCase() {
+    let contract: CreateCaseContract = new CreateCaseContract(this.createCaseData)  
+    console.log(contract)
+    this.bpmProcessService.createCase(this.processDefinition.id, contract)
+      .subscribe(
+        createCaseSuccessResponse => {
+          this.createCaseSuccessResponse = createCaseSuccessResponse
+          this.passedTestBPMProcessCreateCase = true
+        },
+        errorResponse => this.errorResponse = errorResponse
+      )
+
+  }
+
 }
