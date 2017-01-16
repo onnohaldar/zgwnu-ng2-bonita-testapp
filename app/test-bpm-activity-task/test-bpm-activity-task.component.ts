b59a6f117bb5471d649c4a2d16f450ae7777db82
Onno Haldar <onno@zgw.nu>
2017-01-16 22:41:50 +0100

    add new task tests

--- app/test-bpm-activity-task/test-bpm-activity-task.component.ts
+++ app/test-bpm-activity-task/test-bpm-activity-task.component.ts
@@ -30,6 +30,14 @@ export class TestBpmActivityTaskComponent implements OnInit {
     passedTestBPMHumanTaskSearchHumanTasks: boolean = false
     passedTestBPMHumanTaskGetHumanTask: boolean = false
 
+    // task test vars
+    task: BonitaTask
+    passedTestBPMTaskSearchTasks: boolean = false
+    passedTestBPMTaskGetTask: boolean = false
+
+    // user task test vars
+    userTask: BonitaTask
+    passedTestBPMUserTaskGetUserTask: boolean = false
 
     constructor(
         private bpmActivityService: BonitaBpmActivityService, 
@@ -44,67 +52,111 @@ export class TestBpmActivityTaskComponent implements OnInit {
         console.log('InitTestBpmActivityTaskComponent')
 
         // start testchain
-        this.BPMActivitySearchActivities()
+        this.bpmActivitySearchActivities()
 
     }
 
-    private BPMActivitySearchActivities() {
+    private bpmActivitySearchActivities() {
         let bonitaSearchParms: BonitaSearchParms = new BonitaSearchParms(0, 1)
         bonitaSearchParms.filters = ['name=Change Data Task']
 
         this.bpmActivityService.searchActivities(bonitaSearchParms)
             .subscribe(
-                activity => {
-                    this.activity = activity[0]
+                activities => {
+                    this.activity = activities[0]
                     this.passedTestBPMActivitySearchActivities = true
                     // next test in chain (1)
-                    this.BPMActivityGetActivity()
+                    this.bpmActivityGetActivity()
                 },
                 errorResponse => this.errorResponse = errorResponse
             )
     }
 
-    private BPMActivityGetActivity() {
+    private bpmActivityGetActivity() {
         this.bpmActivityService.getActivity(this.activity.id)
             .subscribe(
                 activity => {
                     this.activity = activity
                     this.passedTestBPMActivityGetActivity = true
                     // next test in chain (2)
-                    this.BPMHumanTaskSearchHumanTasks()
+                    this.bpmHumanTaskSearchHumanTasks()
                 },
                 errorResponse => this.errorResponse
             )
     }
 
-    private BPMHumanTaskSearchHumanTasks() {
+    private bpmHumanTaskSearchHumanTasks() {
         let bonitaSearchParms: BonitaSearchParms = new BonitaSearchParms(0, 1)
         bonitaSearchParms.filters = ['name=Change Data Task']
 
         this.bpmHumanTaskService.searchHumanTasks(bonitaSearchParms)
             .subscribe(
-                humanTask => {
-                    this.humanTask = humanTask[0]
+                humanTasks => {
+                    console.log(humanTasks)
+                    this.humanTask = humanTasks[0]
                     this.passedTestBPMHumanTaskSearchHumanTasks = true
                     // next test in chain (3)
-                    this.BPMHumanTaskGetHumanTask()
+                    this.bpmHumanTaskGetHumanTask()
                 },
                 errorResponse => this.errorResponse = errorResponse
             )
     }
 
-    private BPMHumanTaskGetHumanTask() {
+    private bpmHumanTaskGetHumanTask() {
         this.bpmHumanTaskService.getHumanTask(this.humanTask.id)
             .subscribe(
                 humanTask => {
                     this.humanTask = humanTask
                     this.passedTestBPMHumanTaskGetHumanTask = true
                     // next test in chain (4)
-
+                    this.bpmTaskSearchTasks()
                 },
                 errorResponse => this.errorResponse
             )
     }
 
+    private bpmTaskSearchTasks() {
+        let bonitaSearchParms: BonitaSearchParms = new BonitaSearchParms(0, 1)
+        bonitaSearchParms.filters = ['name=Change Data Task']
+
+        this.bpmTaskService.searchTasks(bonitaSearchParms)
+            .subscribe(
+                tasks => {
+                    console.log(tasks)
+                    this.task = tasks[0]
+                    this.passedTestBPMTaskSearchTasks = true
+                    // next test in chain (3)
+                    this.bpmTaskGetTask()
+                },
+                errorResponse => this.errorResponse = errorResponse
+            )
+    }
+
+    private bpmTaskGetTask() {
+        this.bpmTaskService.getTask(this.task.id)
+            .subscribe(
+                task => {
+                    this.task = task
+                    this.passedTestBPMTaskGetTask = true
+                    // next test in chain (4)
+                    this.bpmUserTaskGetUserTask()
+                },
+                errorResponse => this.errorResponse
+            )
+    }
+
+
+    private bpmUserTaskGetUserTask() {
+        this.bpmUserTaskService.getUserTask(this.task.id)
+            .subscribe(
+                userTask => {
+                    this.userTask = userTask
+                    this.passedTestBPMUserTaskGetUserTask = true
+                    // next test in chain (4)
+
+                },
+                errorResponse => this.errorResponse
+            )
+    }
 
 }
